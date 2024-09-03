ec4eef86a48e9da3b9ef12f3f42693a4882d2bee
P Deepanshu <97436890+deep60@users.noreply.github.com>
2024-09-04 02:59:04 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
+use core::panic;
 use std::env;
 use std::io;
 use std::process;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else if pattern == "\\d" {
-        return input_line.contains(|c: char| c.is_digit(10));
-    } else {
+    if pattern.chars().count() == 0 {
         panic!("Unhandled pattern: {}", pattern)
+    }
+
+    match pattern {
+        "\\d" => input_line.contains(|c: char| c.is_digit(10)),
+        "\\w" => input_line.chars().any(|c| c.is_alphanumeric()),
+        pattern => {
+            if pattern.chars().count() != 1 {
+                panic!("Unhandled pattern: {}", pattern)
+            }
+
+            let input_c = pattern.chars().nth(0).unwrap();
+            input_line.contains(input_c)
+        }
     }
 }
 
