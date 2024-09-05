887274b0fd50d5f656e113b8c7f3312ff26cc236
P Deepanshu <97436890+deep60@users.noreply.github.com>
2024-09-05 10:55:12 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -35,142 +35,208 @@ use std::str::Chars;
 //     }
 // }
 
-#[derive(Debug)]
-enum Pattern {
-    Literal(char),
-    Digit,
-    Alphanumeric,
-    Group(bool, String),
-}
-fn match_literal(chars: &mut Chars, literal: char) -> bool {
-    let c = chars.next();
-    c.is_some_and(|c| c == literal)
-}
-fn match_digit(chars: &mut Chars) -> bool {
-    let c = chars.next();
-    if c.is_none() {
-        return false;
-    }
-    c.unwrap().is_digit(10)
-}
-fn match_alphanumeric(chars: &mut Chars) -> bool {
-    let c = chars.next();
-    c.is_some_and(|c| c.is_alphanumeric())
-}
-fn match_group(chars: &mut Chars, group: &str) -> bool {
-    let c = chars.next();
-    c.is_some_and(|c| group.contains(c))
-}
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    let patterns = build_patterns(pattern);
-    let input_line = input_line.trim_matches('\n');
-    'input_iter: for i in 0..input_line.len() {
-        let input = &input_line[i..];
-        let mut iter = input.chars();
-        for pattern in patterns.iter() {
-            match pattern {
-                Pattern::Literal(l) => {
-                    if !match_literal(&mut iter, *l) {
-                        continue 'input_iter;
-                    }
-                }
-                Pattern::Digit => {
-                    if !match_digit(&mut iter) {
-                        continue 'input_iter;
-                    }
-                }
-                Pattern::Alphanumeric => {
-                    if !match_alphanumeric(&mut iter) {
-                        continue 'input_iter;
-                    }
-                }
-                Pattern::Group(positive, group) => {
-                    if match_group(&mut iter, group) != *positive {
-                        continue 'input_iter;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-    return false;
-}
-fn build_group_pattern(iter: &mut Chars) -> (bool, String) {
-    let mut group = String::new();
-    let mut positive = true;
-    if iter.clone().next().is_some_and(|c| c == '^') {
-        positive = false;
-        iter.next();
-    }
-    loop {
-        let member = iter.next();
-        if member.is_none() {
-            panic!("Incomplete character group");
-        }
-        let member = member.unwrap();
-        if member != ']' {
-            group.push(member);
-            continue;
-        }
-        break;
-    }
-    (positive, group)
-}
-fn build_patterns(pattern: &str) -> Vec<Pattern> {
-    let mut iter = pattern.chars();
-    let mut patterns = Vec::new();
-    loop {
-        let current = iter.next();
-        if current.is_none() {
-            break;
-        }
-        patterns.push(match current.unwrap() {
-            '\\' => {
-                let special = iter.next();
-                if special.is_none() {
-                    panic!("Incomplete special character")
-                }
-                match special.unwrap() {
-                    'd' => Pattern::Digit,
-                    'w' => Pattern::Alphanumeric,
-                    '\\' => Pattern::Literal('\\'),
-                    _ => panic!("Invalid special character"),
-                }
-            }
-            '[' => {
-                let (positive, group) = build_group_pattern(&mut iter);
-                Pattern::Group(positive, group)
-            }
-            l => Pattern::Literal(l),
-        })
-    }
-    patterns
-}
+// #[derive(Debug)]
+// enum Pattern {
+//     Literal(char),
+//     Digit,
+//     Alphanumeric,
+//     Group(bool, String),
+// }
+// fn match_literal(chars: &mut Chars, literal: char) -> bool {
+//     let c = chars.next();
+//     c.is_some_and(|c| c == literal)
+// }
+// fn match_digit(chars: &mut Chars) -> bool {
+//     let c = chars.next();
+//     if c.is_none() {
+//         return false;
+//     }
+//     c.unwrap().is_digit(10)
+// }
+// fn match_alphanumeric(chars: &mut Chars) -> bool {
+//     let c = chars.next();
+//     c.is_some_and(|c| c.is_alphanumeric())
+// }
+// fn match_group(chars: &mut Chars, group: &str) -> bool {
+//     let c = chars.next();
+//     c.is_some_and(|c| group.contains(c))
+// }
+// fn match_pattern(input_line: &str, pattern: &str) -> bool {
+//     let patterns = build_patterns(pattern);
+//     let input_line = input_line.trim_matches('\n');
+//     'input_iter: for i in 0..input_line.len() {
+//         let input = &input_line[i..];
+//         let mut iter = input.chars();
+//         for pattern in patterns.iter() {
+//             match pattern {
+//                 Pattern::Literal(l) => {
+//                     if !match_literal(&mut iter, *l) {
+//                         continue 'input_iter;
+//                     }
+//                 }
+//                 Pattern::Digit => {
+//                     if !match_digit(&mut iter) {
+//                         continue 'input_iter;
+//                     }
+//                 }
+//                 Pattern::Alphanumeric => {
+//                     if !match_alphanumeric(&mut iter) {
+//                         continue 'input_iter;
+//                     }
+//                 }
+//                 Pattern::Group(positive, group) => {
+//                     if match_group(&mut iter, group) != *positive {
+//                         continue 'input_iter;
+//                     }
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+//     return false;
+// }
+// fn build_group_pattern(iter: &mut Chars) -> (bool, String) {
+//     let mut group = String::new();
+//     let mut positive = true;
+//     if iter.clone().next().is_some_and(|c| c == '^') {
+//         positive = false;
+//         iter.next();
+//     }
+//     loop {
+//         let member = iter.next();
+//         if member.is_none() {
+//             panic!("Incomplete character group");
+//         }
+//         let member = member.unwrap();
+//         if member != ']' {
+//             group.push(member);
+//             continue;
+//         }
+//         break;
+//     }
+//     (positive, group)
+// }
+// fn build_patterns(pattern: &str) -> Vec<Pattern> {
+//     let mut iter = pattern.chars();
+//     let mut patterns = Vec::new();
+//     loop {
+//         let current = iter.next();
+//         if current.is_none() {
+//             break;
+//         }
+//         patterns.push(match current.unwrap() {
+//             '\\' => {
+//                 let special = iter.next();
+//                 if special.is_none() {
+//                     panic!("Incomplete special character")
+//                 }
+//                 match special.unwrap() {
+//                     'd' => Pattern::Digit,
+//                     'w' => Pattern::Alphanumeric,
+//                     '\\' => Pattern::Literal('\\'),
+//                     _ => panic!("Invalid special character"),
+//                 }
+//             }
+//             '[' => {
+//                 let (positive, group) = build_group_pattern(&mut iter);
+//                 Pattern::Group(positive, group)
+//             }
+//             l => Pattern::Literal(l),
+//         })
+//     }
+//     patterns
+// }
 
-// Usage: echo <input_text> | your_program.sh -E <pattern>
+// // Usage: echo <input_text> | your_program.sh -E <pattern>
+// fn main() {
+//     // You can use print statements as follows for debugging, they'll be visible when running tests.
+//     //println!("Logs from your program will appear here!");
+
+//     if env::args().nth(1).unwrap() != "-E" {
+//         println!("Expected first argument to be '-E'");
+//         process::exit(1);
+//     }
+
+//     let pattern = env::args().nth(2).unwrap();
+//     let mut input_line = String::new();
+
+//     io::stdin().read_line(&mut input_line).unwrap();
+
+//     // Uncomment this block to pass the first stage
+//     // if match_pattern(&input_line, &pattern) {
+//     //     process::exit(0)
+//     // } else {
+//     //     process::exit(1)
+//     // }
+//     if match_pattern(&input_line, &pattern) {
+//         process::exit(0)
+//     } else {
+//         process::exit(1)
+//     }
+// }
+
+fn match_pattern(input: &str, pattern: &str) -> bool {
+    let mut input = input.bytes();
+    let mut pattern = pattern.as_bytes();
+    let mut had_match = false;
+    let mut had_match = consume_if_starts_with(&mut pattern, b"^");
+    while pattern.len() > 0 {
+        let Some(at) = input.next() else {
+            return false;
+        };
+        let mut old_pattern = pattern;
+        let old_pattern = pattern;
+        let matched;
+        if consume_if_starts_with(&mut pattern, b"\\d") {
+            matched = at.is_ascii_digit();
+        } else if consume_if_starts_with(&mut pattern, b"\\w") {
+            matched = at.is_ascii_alphanumeric() || at == b'_';
+        } else if consume_if_starts_with(&mut pattern, b"[") {
+            let negative = consume_if_starts_with(&mut pattern, b"^");
+            let mut chars = vec![];
+            while !consume_if_starts_with(&mut pattern, b"]") {
+                chars.push(consume(&mut pattern));
+            }
+            matched = negative ^ chars.contains(&at);
+        } else {
+            let expected = consume(&mut pattern);
+            matched = at == expected;
+        }
+        if !matched {
+            if had_match {
+                return false;
+            } else {
+                pattern = old_pattern;
+            }
+        }
+        had_match = matched;
+    }
+    return had_match;
+    #[inline]
+    fn consume(str: &mut &[u8]) -> u8 {
+        // @todo: error handling.
+        let result = str[0];
+        *str = &str[1..];
+        return result;
+    }
+    #[inline]
+    fn consume_if_starts_with(str: &mut &[u8], pat: &[u8]) -> bool {
+        if str.starts_with(pat) {
+            *str = &str[pat.len()..];
+            return true;
+        } else {
+            false
+        }
+    }
+}
+// Usage: echo <input_text> | your_grep.sh -E <pattern>
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    //println!("Logs from your program will appear here!");
-
-    if env::args().nth(1).unwrap() != "-E" {
+    if std::env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
-        process::exit(1);
+        std::process::exit(1);
     }
-
-    let pattern = env::args().nth(2).unwrap();
+    let pattern = std::env::args().nth(2).unwrap();
     let mut input_line = String::new();
-
-    io::stdin().read_line(&mut input_line).unwrap();
-
-    // Uncomment this block to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
-    if match_pattern(&input_line, &pattern) {
-        process::exit(0)
-    } else {
-        process::exit(1)
-    }
+    std::io::stdin().read_line(&mut input_line).unwrap();
+    std::process::exit(!match_pattern(&input_line, &pattern) as i32)
 }
